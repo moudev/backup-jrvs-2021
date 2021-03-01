a11d84b14403a32fe1af9d3b464d5016a7959f69
moudev <rigobertomauriciomartinez@gmail.com>
2021-03-01 17:05:11 -0600

    styles: update list items on the home page

--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,17 @@
 <template>
-  <div class="container mx-auto">
-    <h1>JRVs: {{ jrvs.length }}</h1>
+  <div class="container mx-auto pt-16">
+    <h1 class="text-3xl text-center font-bold">
+      JRV: {{ jrvs.length }} respaldadas
+    </h1>
     <div class="flex flex-wrap justify-center items-center">
       <nuxt-link
         v-for="jrv in jrvs"
         :key="jrv.jrv"
         :to="`jrvs/${jrv.jrv}`"
-        class="flex flex-col items-center border m-8 p-16"
+        class="flex flex-col items-center border m-4 my-8 p-8 md:mx-8"
       >
-        <p>{{ jrv.jrv }}</p>
-        <p>{{ jrv.images.length }} Imágenes</p>
+        <p class="text-xl font-semibold">#{{ jrv.jrv }}</p>
+        <p class="text-xs text-gray-600">{{ jrv.images.length }} Imágenes</p>
       </nuxt-link>
     </div>
   </div>
