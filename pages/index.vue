9ec66b56c7024340ec7243fc2f0a916f60dac3d5
moudev <rigobertomauriciomartinez@gmail.com>
2021-03-01 00:39:53 -0600

    feat: update index to get jrvs from hackerspace

--- pages/index.vue
+++ pages/index.vue
@@ -1,68 +1,32 @@
 <template>
-  <div class="container">
-    <div>
-      <Logo />
-      <h1 class="title">backup-jrvs-2021</h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
+  <div class="container mx-auto">
+    <h1>JRVs: {{ jrvs.length }}</h1>
+    <div class="flex flex-wrap justify-center items-center">
+      <div
+        v-for="jrv in jrvs"
+        :key="jrv.jrv"
+        class="flex flex-col items-center border m-8 p-16"
+      >
+        <p>{{ jrv.jrv }}</p>
+        <p>{{ jrv.images.length }} Imágenes</p>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-export default {}
+import { getJrvs } from '@/utils/utils'
+
+export default {
+  async asyncData({ $axios }) {
+    const data = await $axios.$get(
+      'https://io.hackerspace.sv/data/actas_tse/disponibles.txt'
+    )
+    const jrvs = getJrvs(data)
+
+    return {
+      jrvs,
+    }
+  },
+}
 </script>
-
-<style>
-/* Sample `apply` at-rules with Tailwind CSS
-.container {
-@apply min-h-screen flex justify-center items-center text-center mx-auto;
-}
-*/
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
-</style>
